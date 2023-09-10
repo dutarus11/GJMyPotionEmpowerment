5745dde2e77126818e1db34569b46847df858b21
dutarus11 <dutarus@gmail.com>
2023-09-10 15:50:39 -0700

    4th Commit
    character movement 1

--- Potion My Empowerment Game Jam/Assets/Jesse's Scripts/PlayerScript.cs
+++ Potion My Empowerment Game Jam/Assets/Jesse's Scripts/PlayerScript.cs
@@ -4,9 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace QuickStart
-{
+{    
     public class PlayerScript : NetworkBehaviour
     {
+        public Animator playerAnim;
+        public Rigidbody playerRigid;
+        public float w_speed, wb_speed, olw_speed, rn_speed, ro_speed;
+        public bool running;
+        public Transform playerTransform;
         //public GameObject projecilePoolObj;
 
         //ProjectilePooling projectilePooling;
@@ -18,17 +23,70 @@ namespace QuickStart
 
         private void Start()
         {
-          // proPooling.SetActive(false);
+          
+        }
+
+        private void FixedUpdate()
+        {
+            if (Input.GetKey(KeyCode.W)) 
+            {
+                playerRigid.velocity = transform.forward * w_speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                playerRigid.velocity = -transform.forward * w_speed * Time.deltaTime;
+            }
         }
         void Update()
         {
-            if (!isLocalPlayer) { return; }
+            //if (!isLocalPlayer) { return; }
 
-            float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 110.0f;
-            float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * 4f;
+            //float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 110.0f;
+            //float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * 4f;
 
-            transform.Rotate(0, moveX, 0);
-            transform.Translate(0, 0, moveZ);
+            //transform.Rotate(0, moveX, 0);
+            //transform.Translate(0, 0, moveZ);
+
+            if (Input.GetKeyDown(KeyCode.W)) 
+            {
+                playerAnim.SetTrigger("Run");
+                playerAnim.ResetTrigger("Idle");
+                running = true;
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                playerAnim.SetTrigger("Run");
+                playerAnim.ResetTrigger("Idle");
+                running = false;
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                playerAnim.SetTrigger("Backwards");
+                playerAnim.ResetTrigger("Idle");
+                running = true;
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                playerAnim.SetTrigger("Backwards");
+                playerAnim.ResetTrigger("Idle");
+                running = false;
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                playerTransform.Rotate(0, -ro_speed * Time.deltaTime, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                playerTransform.Rotate(0, ro_speed * Time.deltaTime, 0);
+            }
+            if (running == true)
+            {
+                if (Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    w_speed = w_speed + rn_speed;
+                }
+            }
+
             InputShootingManager();
         }
 
@@ -39,7 +97,9 @@ namespace QuickStart
                 //proPooling.SetActive(true);
                 //ProjectilePooling.isActiveObj = true;
                 //shoot attack power 
+                
                 PlayerAttackPower.Instance.Shoot();               
+                              
             }
         }
     }
--- Potion My Empowerment Game Jam/Assets/Jesse's Scripts/PowerUp.cs
+++ Potion My Empowerment Game Jam/Assets/Jesse's Scripts/PowerUp.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
 public class PowerUp : MonoBehaviour
 {
+    //public GameObject activateObj;
+    //public float seconds = 3f;
+    private void Start()
+    {
+        
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -14,4 +21,17 @@ public class PowerUp : MonoBehaviour
         }
 
     }
+    //void Update()
+    //{
+    //    activateObj.SetActive(true);
+    //    PlayerAttackPower.Instance.Shoot();
+    //    IEnumerator Waiting()
+    //    {
+    //        yield return new WaitForSeconds(seconds);
+
+    //        activateObj.SetActive(false);
+
+    //    }
+    //}     
+   
 }
