c92b567f90fb55b3640198141a6e09a344a3ffcd
dutarus11 <dutarus@gmail.com>
2023-09-25 12:59:32 -0700

    11th Commit

---
+++ Potion My Empowerment Game Jam/Assets/Jesse's Scripts/EnemyController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyController : MonoBehaviour
+{
+    
+    public float findRadius = 10;
+
+    Transform target;
+    NavMeshAgent agent;
+
+
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if (distance < findRadius)
+        {
+            agent.SetDestination(target.position);
+            if (distance <= agent.stoppingDistance)
+            {
+                //attack player
+                //face the player 
+                FacePlayer();
+            }
+        }
+    }
+
+    void FacePlayer()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, findRadius);
+    }
+}
