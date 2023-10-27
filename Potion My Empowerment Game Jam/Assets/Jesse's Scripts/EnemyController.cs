0d5da4f201d624c52ae9c765d73125322d9ac024
dutarus11 <dutarus@gmail.com>
2023-10-27 16:12:56 -0700

    24th Commit
    Enemy AI Completed

--- Potion My Empowerment Game Jam/Assets/Jesse's Scripts/EnemyController.cs
+++ Potion My Empowerment Game Jam/Assets/Jesse's Scripts/EnemyController.cs
@@ -4,59 +4,112 @@ using UnityEngine;
 using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
-    
-    public float findRadius = 10;
-    public float y = 0f;
-    public float num = 10f;
 
-    Transform target;
-    //Transform target;
+    Transform player;
     NavMeshAgent agent;
-
+    public float checkRate = 1f;
+    float nextCheck;
 
     void Start()
     {
-        target = PlayerManager.instance.player.transform;
-        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        agent.updateRotation = true; // rotate the agent always to the player
+        agent.stoppingDistance = agent.radius * 2f; // we do this so the agent dont move the player away
+        InvokeRepeating("FollowPlayer", checkRate, checkRate); // follows the player again every checkRate seconds
     }
 
-    
-    void Update()
+    void FollowPlayer()
     {
-        EnemyMovement();
+        agent.SetDestination(player.position);  // should do the job if your navmesh is setup correctly
     }
+    //Transform playerTransform;
+    //UnityEngine.AI.NavMeshAgent myNavmesh;
+    //public float checkRate = 0.01f;
+    //float nextCheck;
 
-    //Enemy movement 
-    void EnemyMovement()
-    {
-        float distance = Vector3.Distance(target.position, transform.position);
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    if (GameObject.FindGameObjectWithTag("Player").activeInHierarchy)
+    //        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
 
-        if (distance <= findRadius)
-        {
-            agent.SetDestination(target.position);
-            if (distance <= agent.stoppingDistance)
-            {
-                //attack player
-                //face the player 
-                FacePlayer();
-            }
-        }
 
-        transform.position = Vector3.MoveTowards(this.transform.position, target.position, num * Time.deltaTime);
-    }
+    //    myNavmesh = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    //}
 
-    //Enemy facing the player/target 
-    void FacePlayer()
-    {
-        Vector3 direction = (target.position - transform.position).normalized;
-        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, y, direction.z));
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (Time.time > nextCheck)
+    //        nextCheck = Time.time + checkRate;
+    //    FollowPlayer();
+    //}
 
-    //Visual radius of the enemy 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, findRadius);
-    }
+    //void FollowPlayer()
+    //{
+    //    myNavmesh.transform.LookAt(playerTransform);
+    //    myNavmesh.destination = playerTransform.position;
+    //}
+
+    //public float findRadius = 10;
+    //public float y = 0f;
+    //public float num = 10f;
+
+    //Transform target;
+    ////Transform target;
+    //NavMeshAgent agent;
+
+    //public NavMeshAgent enemy;
+    //public Transform player;
+
+    //void Start()
+    //{
+    //    //target = PlayerManager.instance.player.transform;
+    //    //agent = GetComponent<NavMeshAgent>();
+    //}
+
+
+    //void Update()
+    //{
+    //    EnemyMovement();
+    //}
+
+    ////Enemy movement 
+    //void EnemyMovement()
+    //{
+    //    enemy.SetDestination(player.position);
+    //    //float distance = Vector3.Distance(target.position, transform.position);
+
+    //    //if (distance <= findRadius)
+    //    //{
+    //    //    agent.SetDestination(target.position);
+    //    //    if (distance <= agent.stoppingDistance)
+    //    //    {
+    //    //        //attack player
+    //    //        //face the player 
+    //    //        FacePlayer();
+    //    //    }
+    //    //}
+    //    //if (distance == null)
+    //    //{
+    //    //    Debug.Log("NO DISTANCE VALUE FOUND!!");
+    //    //}
+    //    //transform.position = Vector3.MoveTowards(this.transform.position, target.position, num * Time.deltaTime);
+    //}
+
+    ////Enemy facing the player/target 
+    ////void FacePlayer()
+    ////{
+    ////    Vector3 direction = (target.position - transform.position).normalized;
+    ////    Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, y, direction.z));
+    ////    transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    ////}
+
+    ////Visual radius of the enemy 
+    ////private void OnDrawGizmosSelected()
+    ////{
+    ////    Gizmos.color = Color.yellow;
+    ////    Gizmos.DrawWireSphere(transform.position, findRadius);
+    ////}
 }
